e2f3115c545ccf73753ed9f3177611c767a10043
Mingu Chu <mingu@Mingus-MacBook-Pro.local>
2016-03-07 23:38:55 -0600

    first commit

--- Tax calculator/ViewController.m
+++ Tax calculator/ViewController.m
@@ -10,14 +10,55 @@
 
 @interface ViewController ()
 
+@property (weak, nonatomic) IBOutlet UITextField *priceTextField;
+@property (weak, nonatomic) IBOutlet UISegmentedControl *segmentControl;
+@property double sfTax;
+@property double chiTax;
+@property double nyTax;
+@property (weak, nonatomic) IBOutlet UILabel *resultLabel;
+
+
 @end
 
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    self.sfTax = .0875;
+    self.chiTax = .1025;
+    self.nyTax = .08875;
+
+    
 }
+
+- (IBAction)onCalculateButtonTapped:(id)sender {
+    
+    int price = self.priceTextField.text.intValue;
+    
+    double total = 0;
+    
+    if (self.segmentControl.selectedSegmentIndex == 0)
+    {
+        total = self.sfTax * price;
+    }
+    
+    else if (self.segmentControl.selectedSegmentIndex == 1)
+    {
+        total = self.chiTax * price;
+        
+    }
+    
+    else
+    {
+        total = self.nyTax * price;
+    }
+    
+    self.resultLabel.text = [NSString stringWithFormat:@"%.2f", total];
+    
+    
+}
+
+
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
