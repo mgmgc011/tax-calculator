9f415d99a4b7c40ed61ecce1f5ed755bb0f3b4ab
Mingu Chu <mingu@mingus-macbook-pro.local>
2016-03-07 18:12:25 -0600

    Initial Commit

---
+++ Tax calculator/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Tax calculator
+//
+//  Created by Mingu Chu on 3/7/16.
+//  Copyright © 2016 Mingu Chu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Tax calculator/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Tax calculator
+//
+//  Created by Mingu Chu on 3/7/16.
+//  Copyright © 2016 Mingu Chu. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
